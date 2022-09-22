d7fe8400ed265b9bcd75c5b2141679601c8f7952
maribedaris <107567530+maribedaris@users.noreply.github.com>
2022-09-22 16:34:43 -0500

    Proyecto actualizado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { ContentComponent } from './components/content/content.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ToolbarComponent,
+    ContentComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule
